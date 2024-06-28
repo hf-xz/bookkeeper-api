48741a6b9d91ea99886fe637922f5511af636b25
hfxz <xuan__zai@outlook.com>
2024-06-29 01:26:02 +0800

    修改环境变量格式

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # 记账工具-后端
 
 使用 FastAPI + MongoDB
+
+## 本地安装运行
+
+1. 使用喜欢的方式创建虚拟环境
+
+```sh
+conda create -n fastapi python=3.12 -y
+conda activate fastapi
+```
+
+2. 安装依赖
+
+```sh
+pip install fastapi pymongo
+```
+
+3. 配置环境变量
+
+配置以下环境变量（配置到项目根目录的 `.env` 文件即可）
+
+```text
+MONGODB_URL=<connect_string>
+MONGODB_DATABASE=<database_name>
+```
+
+4. 运行
+
+```sh
+python app.py
+```
--- database.py
+++ database.py
@@ -6,12 +6,10 @@ from pymongo import MongoClient
 load_dotenv()
 
 MONGODB_URL = getenv('MONGODB_URL')
-MONGODB_AUTH = getenv('MONGODB_AUTH')
-CONNECT_STRING = f'mongodb+srv://{MONGODB_AUTH}@{MONGODB_URL}'
 MONGODB_DATABASE = getenv('MONGODB_DATABASE')
 
 def get_database():
-    client = MongoClient(CONNECT_STRING)
+    client = MongoClient(MONGODB_URL)
     database = client.get_database(MONGODB_DATABASE)
     return database
 
